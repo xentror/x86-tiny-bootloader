5292a9ba8663c436607824d08f27f94c4ed6ef3f
Thibault vivies <thibault.vivies@epita.fr>
2020-10-26 21:26:51 +0100

    hello_world: print a warm welcome and halte.

--- hello_world/boot.asm
+++ hello_world/boot.asm
@@ -1,13 +1,55 @@
 _start:
     mov ax, 0x7C0
-    mov ds, ax
-    mov ax, 0x7E0
+    mov ds, ax ; put data segment at same address as cs
+    mov es, ax ; put extra segment at same address as cs
+    mov ax, 0x7E0 ; create stack 512 bytes after cs -- next sector
     mov ss, ax
 
-    mov sp, 0x2000
+    mov sp, 0x2000 ; instanciante stack pointer
 
-    call  clear_window
+    call clear_window
+    call reset_cursor
+    call write_string
+
     hlt
+
+reset_cursor:
+    push bp
+    mov bp, sp
+    pusha
+
+    mov ah, 0x02
+    mov bh, 0x0
+    mov dh, 0x0
+    mov dl, 0x0
+    int 0x10
+
+    popa
+    mov sp, bp
+    pop bp
+    ret
+
+write_string:
+    push bp
+    mov bp, sp
+    pusha
+
+    mov ah, 0x13
+    mov al, 0x1 ; update cursor after writing
+    mov bh, 0x0
+    mov bl, 0x02
+    mov cx, msg_end - msg
+    mov dx, 0x0
+
+    push bp
+    mov bp, msg
+    int 0x10
+    pop bp
+
+    popa
+    mov sp, bp
+    pop bp
+    ret
 
 clear_window:
     push bp
@@ -27,6 +69,9 @@ clear_window:
     mov sp, bp
     pop bp
     ret
+
+msg: db "Vas niquer ta mere !"
+msg_end: db 0
 
 times  510-($-$$) db 0
 dw 0xAA55
