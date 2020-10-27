b765be69c2bf5e4743341c06d9961d0e78e5a2ab
Thibault vivies <thibault.vivies@epita.fr>
2020-10-27 16:42:37 +0100

    protected_boot: switch to protected mode, and jump on 32 bit code. Print a char.

--- protected_hello/boot.asm
+++ protected_hello/boot.asm
@@ -1,5 +1,58 @@
 bits 16
-org 0x7C00
+
+_start:
+    mov ax, 0x7C0
+    mov ds, ax ; put data segment at same address as cs
+    mov es, ax ; put extra segment at same address as cs
+    mov ax, 0x7E0 ; create stack 512 bytes after cs -- next sector
+    mov ss, ax
+
+    mov sp, 0x2000 ; instanciante stack pointer
+
+    call clear_window
+
+    ; Enable A20 bits
+    mov ax, 0x2401
+    int 0x15
+
+    ; calculate the physical address of gdt
+    xor eax, eax
+    mov ax, ds ; load the ds content
+    shl eax, 4 ; mult by 16, convert to physical addr -> shift of 4
+    add ax, gdt_start
+    mov [gdt_ptr], eax
+
+    ; load gdt descriptor in gdtr
+    lgdt  [gdt_descriptor]
+    ; set protected mode bit in cr0
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+
+    ; Jump to 32bit code - relatif to new code segment
+    jmp CODE_SEGMENT: 0x7c00 + boot2
+
+clear_window:
+    push bp
+    mov bp, sp
+    pusha
+
+    mov ah, 0x07
+    xor al, al
+    mov bh, 0x07
+    mov ch, 0x0
+    mov cl, 0x0
+    mov dh, 0x18
+    mov dl, 0x4f
+    int 0x10
+
+    popa
+    mov sp, bp
+    pop bp
+    ret
+
+msg: db "Vas niquer ta mere !"
+msg_end: db 0
 
 ; Declare GDT struct
 ; here access contain s16_19 and access ( 4 + 4 bits)
@@ -34,67 +87,30 @@ iend
 gdt_end:
 
 ; gdt size and pointer to load
-gdt_descriptor:
-    dw gdt_end - gdt_start
-    dd gdt_start
+gdt_descriptor dw gdt_end - gdt_start
+gdt_ptr dd 0 ; pointeur to start of gdt -> will be update later
 CODE_SEGMENT equ gdt_code - gdt_start
 DATA_SEGMENT equ gdt_data - gdt_start
 
-_start:
-    mov ax, 0x7C0
-    mov ds, ax ; put data segment at same address as cs
-    mov es, ax ; put extra segment at same address as cs
-    mov ax, 0x7E0 ; create stack 512 bytes after cs -- next sector
-    mov ss, ax
-
-    mov sp, 0x2000 ; instanciante stack pointer
-
-    call clear_window
-
-    ; Enable A20 bits
-    mov ax, 0x2401
-    int 0x15
-
-    ; load gdt descriptor in gdtr
-    lgdt  [gdt_descriptor]
-    ; set protected mode bit in cr0
-    mov eax, cr0
-    or eax, 0x1
-    mov cr0, eax
-    ; Jump to 32bit code - relatif to new code segment
-    jmp CODE_SEGMENT:boot2
-
-clear_window:
-    push bp
-    mov bp, sp
-    pusha
-
-    mov ah, 0x07
-    xor al, al
-    mov bh, 0x07
-    mov ch, 0x0
-    mov cl, 0x0
-    mov dh, 0x18
-    mov dl, 0x4f
-    int 0x10
-
-    popa
-    mov sp, bp
-    pop bp
-    ret
-
-msg: db "Vas niquer ta mere !"
-msg_end: db 0
-
 bits 32
+
+FarPointer dd 0
 boot2:
     ; Replace segment with correct entry in the gdt
-    mov eax, DATA_SEGMENT
+    mov ax, DATA_SEGMENT
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
+
+    ; write a char to vga text buffer
+    mov ebx, 0xB8000
+    mov al, '!'
+    mov ah, 0x02
+    mov word [ebx], ax
+
+    cli
     hlt
 
 times  510-($-$$) db 0
